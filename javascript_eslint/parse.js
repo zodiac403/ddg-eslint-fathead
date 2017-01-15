38d4aa1136ef813ffbc3b17f87bff6bc04f2277f
zodiac403 <zodiac403@gmx.de>
2017-01-15 10:58:26 +0100

    Code: - Add parse.js - - Tune download folder structure. git commit -m Code:

--- javascript_eslint/parse.js
+++ javascript_eslint/parse.js
@@ -4,9 +4,10 @@ var fs = require('fs');
 var yaml = require('js-yaml');
 var write = require('./lib/write.js');
 
-var inputFile = __dirname + '/download/rules.yml';
+var inputFile = __dirname + '/download/summary/rules.yml';
 var outputFile = './output.txt';
 
 var content = yaml.safeLoad(fs.readFileSync(inputFile));
 
+// todo: Limited on 'Possible Errors' (idx 0) yet:
 write(outputFile, content.categories[0].rules);
